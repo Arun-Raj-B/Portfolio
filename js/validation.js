65677e8315aecb7e009ffac4825b2b9f42c7846a
Arun-Raj-B <sent2arunraj@gmail.com>
2022-08-22 10:46:17 +0530

    validation suuccessful

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,15 @@
+    
+ 
     let username = document.getElementById("username");
     let email = document.getElementById("email");
     let subject = document.getElementById("subject");
     let message = document.getElementById("message");
     
     let error=document.getElementsByClassName("error");
-
+    
 function validate()
 {
-
+    // usernamae validation
     if(username.value == "")
     {
        error[0].innerHTML = "You can't leave this section blank";
@@ -20,22 +22,42 @@ function validate()
         document.submit-form.username.focus();
         return false;
     }
+    else if(username.value.length <= 3)
+    {
+        error[0].innerHTML = "Enter your full name";
+        document.submit-form.username.focus();
+        return false;
+    }
     else
     {
         error[0].innerHTML = "";
     }
 
+    // email validation
     if(email.value == "")
     {
        error[1].innerHTML = "You can't leave this section blank";
        document.submit-form.email.focus();
        return false;
     }
+    else if(email.value.indexOf('@') <= 0)
+    {
+        error[1].innerHTML = "please use an '@' in the email";
+        document.submit-form.email.focus();
+        return false;
+    }
+    else if((email.value.charAt(email.value.length-4)!='.') && (email.value.charAt(email.value.length-3)!='.'))
+    {
+        error[1].innerHTML = "please enter a valid email";
+        document.submit-form.email.focus();
+        return false;
+    }
     else
     {
         error[1].innerHTML = "";
     }
-
+    
+    // Subject validation 
     if(subject.value == "")
     {
        error[2].innerHTML = "You can't leave this section blank";
@@ -47,11 +69,18 @@ function validate()
         error[2].innerHTML = "";
     }
 
+    // message validation
     if(message.value == "")
     {
        error[3].innerHTML = "You can't leave this section blank";
        document.submit-form.message.focus();
        return false;
+    }
+    else if(message.value.length <= 50)
+    {
+        error[3].innerHTML = "Enter minimum of 50 characters";
+        document.submit-form.message.focus();
+        return false;
     }
     else
     {
@@ -60,6 +89,30 @@ function validate()
     
     return true;
 }
+
+// submission
+
+$("#submit-form").submit((e)=>{
+    e.preventDefault();
+    let validation = validate();
+
+    if(validation)
+    {
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbz8WQ00hm8F_qis6Xc1Qbman_Rabdy79Y0ZbL53vJibsRUMKB5DEHBqmgdRx3PIuTJsPA/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+        })}
+})
 
 
 
